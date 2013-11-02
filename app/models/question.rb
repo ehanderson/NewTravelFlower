7ce3771a1171f7743383255c99f5f45fae884ca6
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-02 14:33:28 -0500

    changed migration to allow for tag_names to be set properly

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ActiveRecord::Base
-  attr_accessible :question_text, :title, :user_id, :view_count
+  attr_accessible :question_text, :user_id, :view_count
 
   belongs_to :user
   has_many :answers
-  has_many :tags
+
 
 end
