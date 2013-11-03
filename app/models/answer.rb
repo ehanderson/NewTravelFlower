d70a3b9a19c225d2220a745296cb6d158e4ce6a1
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-03 16:34:00 -0600

    removed creation of tags from controller and gave it to the model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
   attr_accessible :answer_text, :question_id, :user_id, :tag_names
-
+attr_accessor :tag_names
   has_many :taggings
   has_many :tags, through: :taggings
   has_many :votes
@@ -10,18 +10,13 @@ class Answer < ActiveRecord::Base
 
   after_save :assign_tags
 
-  def tag_names
-    @tag_names || tags.map(&:name).join(' ')
-  end
 
   private
 
   def assign_tags
-    if @tag_names
+    p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
       self.tags = @tag_names.split(/\s+/).map do |name|
-        Tag.find_or_create_by_name(name)
-        answer.tags.create_by_name(name)
-      end
-    end
+        Tag.create(:name => name)
+       end
   end
 end
